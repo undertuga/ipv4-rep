ea40f466bba716a1c043da98307a5e67cc3e20bc
undertuga <undertuga@tutanota.de>
2015-04-02 01:10:42 +0100

    1st commit! package structure organization...

---
+++ src/README.md
@@ -0,0 +1,13 @@
+#ipv4-rep
+
+####IPv4 reputation toolkit, developed for use on Node.JS / IO.JS infosec related projects. 
+####(NPM package)
+***
+
+######v0.1.0 available prototypes:
+
+- **generateIPv4(*class, callback*)**
+
+	Generates random IPv4 according to desired class (A to E | R for random class).
+	
+***
---
+++ src/lib/ipv4-rep.js
@@ -0,0 +1,40 @@
+/*
+ * ==============================================================
+ * 						IPv4-REP (v0.1.0)
+ * ==============================================================
+ * 	
+ * 	IPv4 reputation toolkit developed for use on Node.JS / IO.JS
+ * 	infosec related projects 
+ * 
+ * ==============================================================
+ * 
+ * 	License: MIT (check attached file)
+ * 	Author: undertuga[at]tutanota[dot]de
+ * 	GitHub: https://github.com/undertuga/ipv4-rep
+ * 
+ * ==============================================================
+ */
+
+
+
+
+
+
+/* 
+ * IPv4 REP - Prototypes Holder 
+ * ------------------------------
+ */
+
+IPv4Rep = function(){
+	
+	
+};
+
+
+
+
+
+
+
+// exporting IPv4 REP prototypes
+exports.IPv4Rep = IPv4Rep;
