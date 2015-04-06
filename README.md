7ca5dfc46685b8f9bf4b774933372181e2538a9c
undertuga <undertuga@tutanota.de>
2015-04-06 03:22:05 +0100

    final tunes on v0.1.0 source before publishing on NPM

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#ipv4-rep
+#IPv4-REP
 
 ####IPv4 reputation toolkit, developed for use on Node.JS / IO.JS infosec related projects. 
 ####(NPM package)
@@ -6,8 +6,33 @@
 
 ######v0.1.0 available prototypes:
 
-- **generateIPv4(*class, callback*)**
+- **spamhaus(*ipv4, callback*)**
 
-	Generates random IPv4 according to desired class (A to E | R for random class).
+	Check given IPv4 reputation at SPAMHAUS services.
+
+
+- **mailspike(*ipv4, callback*)**
+
+	Check given IPv4 reputation at MAILSPIKE services.
+
+
+- **spamcop(*ipv4, callback*)**
+
+	Check given IPv4 reputation at SPAMCOP services.
+
+
+- **sorbs(*ipv4, callback*)**
+
+	Check given IPv4 reputation at SORBS services.
+
+
+- **cbl(*ipv4, callback*)**
+
+	Check given IPv4 reputation at CBL services.
+
+
+- **unsubscore(*ipv4, callback*)**
+
+	Check given IPv4 reputation at UNSUBSCORE services.
 	
 ***
--- src/lib/ipv4-rep.js
+++ src/lib/ipv4-rep.js
@@ -17,17 +17,45 @@
 
 
 
+/*
+========================
+	IPv4 Reputation 
+	protypes holder
+========================
+*/
+IPv4Rep = function(){};
 
 
 
-/* 
- * IPv4 REP - Prototypes Holder 
- * ------------------------------
- */
 
-IPv4Rep = function(){
+
+
+/*
+========================
+	IPv4 Reputation
+	SPAMHAUS REP CHECK
+========================
+*/
+IPv4Rep.prototype.spamhaus = function(ipv4, callback){
 	
-	
+	// validating gathered data
+	if((typeof(ipv4) === 'undefined') || (ipv4 === null) || (ipv4.length <= 0)){callback(null, false); return;}
+	else{
+		
+		// declaring required external libs
+		var dns = require('dns'), rawIP = ipv4.split(".");
+		
+		// checking spamhaus reputation service
+		dns.resolve4(rawIP[3] + '.' + rawIP[2] + '.' + rawIP[1] + '.' + rawIP[0] + '.zen.spamhaus.org', 
+			
+			function(err, res){
+			
+				// fail safe bail out
+				if(err || !res){callback(null, false); return;}
+				else{callback(null, res);}
+			}
+		);
+	}
 };
 
 
@@ -36,5 +64,187 @@ IPv4Rep = function(){
 
 
 
-// exporting IPv4 REP prototypes
+
+/*
+========================
+	IPv4 Reputation 
+	MAILSPIKE REP CHECK
+========================
+*/
+IPv4Rep.prototype.mailspike = function(ipv4, callback){
+	
+	// validating gathered data
+	if((typeof(ipv4) === 'undefined') || (ipv4 === null) || (ipv4.length <= 0)){callback(null, false); return;}
+	else{
+		
+		// declaring required external libs
+		var dns = require('dns'), rawIP = ipv4.split(".");
+		
+		// checking mailspike reputation service
+		dns.resolve4(rawIP[3] + '.' + rawIP[2] + '.' + rawIP[1] + '.' + rawIP[0] + '.rep.mailspike.net', 
+			
+			function(err, res){
+			
+				// fail safe bail out
+				if(err || !res){callback(null, false); return;}
+				else{callback(null, res);}
+			}
+		);
+	}
+};
+
+
+
+
+
+
+
+
+
+/*
+========================
+	IPv4 Reputation 
+	MAILSPIKE REP CHECK
+========================
+*/
+IPv4Rep.prototype.spamcop = function(ipv4, callback){
+	
+	// validating gathered data
+	if((typeof(ipv4) === 'undefined') || (ipv4 === null) || (ipv4.length <= 0)){callback(null, false); return;}
+	else{
+		
+		// declaring required external libs
+		var dns = require('dns'), rawIP = ipv4.split(".");
+		
+		// checking spamcop reputation service
+		dns.resolve4(rawIP[3] + '.' + rawIP[2] + '.' + rawIP[1] + '.' + rawIP[0] + '.bl.spamcop.net', 
+			
+			function(err, res){
+			
+				// fail safe bail out
+				if(err || !res){callback(null, false); return;}
+				else{callback(null, res);}
+			}
+		);
+	}
+};
+
+
+
+
+
+
+
+
+
+
+/*
+========================
+	IPv4 Reputation 
+	SORBS REP CHECK
+========================
+*/
+IPv4Rep.prototype.sorbs = function(ipv4, callback){
+	
+	// validating gathered data
+	if((typeof(ipv4) === 'undefined') || (ipv4 === null) || (ipv4.length <= 0)){callback(null, false); return;}
+	else{
+		
+		// declaring required holders
+		var dns = require('dns'), rawIP = ipv4.split(".");
+		
+		// checking SORBS reputation service
+		dns.resolve4(rawIP[3] + '.' + rawIP[2] + '.' + rawIP[1] + '.' + rawIP[0] + '.dnsbl.sorbs.net', 
+			
+			function(err, res){
+			
+				// fail safe bail out
+				if(err || !res){callback(null, false); return;}
+				else{callback(null, res);}
+			}
+		);
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+/*
+========================
+	IPv4 Reputation 
+	CBL REP CHECK
+========================
+*/
+IPv4Rep.prototype.cbl = function(ipv4, callback){
+	
+	// validating gathered data
+	if((typeof(ipv4) === 'undefined') || (ipv4 === null) || (ipv4.length <= 0)){callback(null, false); return;}
+	else{
+		
+		// declaring required holders
+		var dns = require('dns'), rawIP = ipv4.split(".");
+		
+		// checking CBL reputation service
+		dns.resolve4(rawIP[3] + '.' + rawIP[2] + '.' + rawIP[1] + '.' + rawIP[0] + '.cbl.abuseat.org', 
+			
+			function(err, res){
+			
+				// fail safe bail out
+				if(err || !res){callback(null, false); return;}
+				else{callback(null, res);}
+			}
+		);
+	}
+};
+
+
+
+
+
+
+
+
+
+/*
+========================
+	IPv4 Reputation 
+	UNSUBSCORE REP CHECK
+========================
+*/
+IPv4Rep.prototype.unsubscore = function(ipv4, callback){
+	
+	// validating gathered data
+	if((typeof(ipv4) === 'undefined') || (ipv4 === null) || (ipv4.length <= 0)){callback(null, false); return;}
+	else{
+		
+		// declaring required holders
+		var dns = require('dns'), rawIP = ipv4.split(".");
+		
+		// checking UNSUBSCORE reputation service
+		dns.resolve4(rawIP[3] + '.' + rawIP[2] + '.' + rawIP[1] + '.' + rawIP[0] + '.ubl.unsubscore.com', 
+			
+			function(err, res){
+			
+				// fail safe bail out
+				if(err || !res){callback(null, false); return;}
+				else{callback(null, res);}
+			}
+		);
+	}
+};
+
+
+
+
+
+
+
+// exporting prototypes
 exports.IPv4Rep = IPv4Rep;
